8f5ca5841d9944e5b296c122b708848e1fb9829f
keit454 <inoue.keitaro454@mail.kyutech.jp>
2023-10-26 23:08:22 +0900

    10/26

--- next-js/app/page.tsx
+++ next-js/app/page.tsx
@@ -1,53 +1,59 @@
-import { AppBar, Box, Button, Card, CardActions, CardContent, CardMedia, IconButton, Toolbar, Typography } from '@mui/material'
-import Image from 'next/image'
-import { pokemons } from './const'
+import {
+  AppBar,
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import Image from "next/image";
+import { pokemons } from "./const";
 
-
-const getImageFileName=(id:number)=>{
-if(id<10){
-  console.log('00'+id+'.png')
-  return '00'+id+'.png'
-}else if(id<100){
-  console.log(id+'.png')
-  return '0'+id+'.png'
-}else{
-  return id+'.png'
-}
-}
+const getImageFileName = (id: number) => {
+  if (id < 10) {
+    return `00${id}.png`;
+  } else if (id < 100) {
+    return `0${id}.png`;
+  } else {
+    return `${id}.png`;
+  }
+};
 
 export default function Home() {
-  return (<>
-  <Box sx={{ flexGrow: 1 }}>
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          僕の作る最強のポケモン図鑑
-        </Typography>
-      </Toolbar>
-    </AppBar>
-  </Box>
-  
- 
-  {pokemons.map((pokemon)=>{
-return ( <Card sx={{ minWidth: 50 }}>
-  <CardContent>
-  <CardMedia
-        sx={{ height: 100,width:100 }}
-        image={getImageFileName(pokemon.id)}
-        title="green iguana"
-      />
-    <Typography variant="body2">
-      {pokemon.name.japanese}
-    </Typography>
-    <Typography variant="body2">
-      {pokemon.type[0]}
-    </Typography>
-  </CardContent>
-  <CardActions>
-    <Button size="small">Learn More</Button>
-  </CardActions>
-</Card>)
-  })}
-  </>
-  )
+  return (
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              僕の作る最強のポケモン図鑑
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Grid container spacing={1}>
+        {pokemons.map((pokemon, index) => (
+          <Grid item key={index}>
+            <Card sx={{ width: 150, height: 250 }}>
+              <CardMedia
+                component="img"
+                image={getImageFileName(pokemon.id)}
+                title="green iguana"
+              />
+              <CardContent>
+                <Typography variant="body2">{pokemon.name.japanese}</Typography>
+                <Typography variant="body2">{pokemon.type[0]}</Typography>
+              </CardContent>
+              <Button size="small">詳細</Button>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </>
+  );
 }
